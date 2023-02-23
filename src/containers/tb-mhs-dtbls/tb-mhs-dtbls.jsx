432702e0c2c3f7234f0e8391ebe20b77353d9eb3
Sulthon <sultona412@gmail.com>
2023-02-23 14:09:57 +0700

    final project

--- src/containers/tb-mhs-dtbls/tb-mhs-dtbls.jsx
+++ src/containers/tb-mhs-dtbls/tb-mhs-dtbls.jsx
@@ -3,32 +3,25 @@ import $ from "jquery";
 import { } from "datatables.net-bs5";
 import "bootstrap/dist/css/bootstrap.css";
 
+
 class TableMahasiswa extends Component {
+
     componentDidMount() {
-        $(() => {
-            $('#table-mhs').DataTable({
-                destroy: true,
-                ajax: {
-                    url: "http://10.28.1.86/restapi-oop/objects/data_mhs.php",
-                    dataSrc: ''
-                },
-                columns: [
-                    { data: "nim" },
-                    { data: "nama" },
-                    { data: "jenis_kelamin" },
-                    { data: "tempat_lahir" },
-                    { data: "tanggal_lahir" },
-                    { data: "alamat" },
-                ],
-            });
-        })
+        const table = $('#table-mhs').DataTable({
+            destroy: true,
+            processing: true,
+            serverSide: true,
+            ajax: {
+                url: 'http://10.28.1.86/restapi-oop/objects/server_side.php',
+            },
+        });
     }
 
     render() {
         return (
-            <div className="container p-4">
+            <div div className="container p-4" >
                 <h1 className="mb-3">Data Mahasiswa</h1>
-                <table id="table-mhs" className="table table-striped">
+                <table id="table-mhs" className="table table-striped w-max">
                     <thead>
                         <tr>
                             <th>NIM</th>
@@ -42,10 +35,9 @@ class TableMahasiswa extends Component {
                     <tbody>
                     </tbody>
                 </table>
-            </div>
+            </div >
         )
     }
 }
-
 
 export default TableMahasiswa;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import TableMahasiswa from './containers/tb-mhs-dtbls/tb-mhs-dtbls';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Home from './containers/home/home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <TableMahasiswa />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
